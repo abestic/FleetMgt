02d8b931e9c04eb41490dc7ad0587fbfb31b017d
A. Bestic <abestic@mac.com>
2013-05-13 17:35:30 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FleetMgt::Application.config.secret_token = '4f82efd6fa6326a4fdd5305cfd1de6752bc90b73f0892c0a7192617c94e54606288f5d0c6bfc1c9da883c7f824fa573cf43c9f964ae051cd6cdef2609201fe89'
